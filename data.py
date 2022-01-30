7dd21d0384a4c61f09271536c9d1d745d289ce17
IceSandwich <cxndream@gmail.com>
2022-01-31 05:51:15 +0800

    use lock to add robustness

--- data.py
+++ data.py
@@ -3,11 +3,16 @@ import bpy
 class ICECS_DT_data(bpy.types.PropertyGroup):
     ResX: bpy.props.IntProperty("Resolution X", default=1920)
     ResY: bpy.props.IntProperty("Resolution Y", default=1080)
+    CameraObjName: bpy.props.StringProperty("Camera Obj Name")
     CameraName: bpy.props.StringProperty("Camera Name")
+    Lock: bpy.props.BoolProperty("Lock for data", default=False)
 
     def timerFunc(self, context:bpy.types.Context):
         if self.TimerEnable:
+            print("start timer")
             bpy.ops.icecs.recover() # start timer
+        else:
+            print("stop timer") # stop by timer
 
     TimerEnable: bpy.props.BoolProperty("Use timer", update=timerFunc, default=False)
 
@@ -28,20 +33,37 @@ class ICECS_DT_data(bpy.types.PropertyGroup):
         return scene.IceCS_data
 
     @classmethod
-    def updateData(cls, context:bpy.types.Context):
-        data = cls.getOrNewProperty(context.scene)
+    def updateData(cls, context:bpy.types.Context, autoLock:bool = False):
+        data:ICECS_DT_data = cls.getOrNewProperty(context.scene)
+        if data.Lock:
+            print("access locked camera data")
+            return
+        if autoLock:
+            data.Lock = True
         data.ResX = context.scene.render.resolution_x
         data.ResY = context.scene.render.resolution_y
-        data.CameraName = context.scene.camera.name if context.scene.camera else ""
-        print("update to %d, %d, %s"%(data.ResX, data.ResY, data.CameraName))
+        data.CameraObjName = context.scene.camera.name if context.scene.camera else ""
+        data.CameraName = context.scene.camera.data.name if context.scene.camera else ""
+        print("update to %d, %d, %s / %s"%(data.ResX, data.ResY, data.CameraObjName, data.CameraName))
 
     @classmethod
-    def recoverData(cls, context:bpy.types.Context):
-        data = context.scene.IceCS_data
+    def recoverData(cls, context:bpy.types.Context, autoLock:bool = False):
+        data:ICECS_DT_data = context.scene.IceCS_data
         context.scene.render.resolution_x = data.ResX
         context.scene.render.resolution_y = data.ResY
-        context.scene.camera = context.blend_data.objects.get(data.CameraName)
-        print("recover to %d, %d, %s"%(data.ResX, data.ResY, data.CameraName))
+        context.scene.camera = context.blend_data.objects.get(data.CameraObjName)
+
+        if context.scene.camera:
+            print("recover to %d, %d, %s"%(data.ResX, data.ResY, data.CameraObjName))
+        else:
+            for x in context.blend_data.objects:
+                if x.type == 'CAMERA' and x.data.name == data.CameraName:
+                    context.scene.camera = x
+                    print("recover to %d, %d, %s (from %s)"%(data.ResX, data.ResY, x.name, data.CameraName))
+                    break
+
+        if autoLock:
+            data.Lock = False
 
 CLASSES = [
     ICECS_DT_data
